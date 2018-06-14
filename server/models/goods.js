877111227b8008f934f7898ae8b9cd9d82252d40
Sojourn2017 <1195110630@qq.com>
2018-06-14 23:25:49 +0800

    GoodList

--- server/models/goods.js
+++ server/models/goods.js
@@ -1,7 +1,7 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
 
-var productSchema = new Schema({
+let productSchema = new Schema({
   "productId": String,
   "productName": String,
   "salePrice": Number,
--- server/routes/goods.js
+++ server/routes/goods.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var Goods = require('../models/goods');
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+let Goods = require('../models/goods');
+let User = require('../models/user');
 
 // 连接mangoDB数据库
 mongoose.connect('mongodb://localhost:27017/market');
@@ -18,7 +19,7 @@ mongoose.connection.on('disconnected',function () {
   console.log('mongodb connected disconnected');
 })
 
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
   let page = parseInt(req.param('page'));
   let pageSize = parseInt(req.param('pageSize'));
   let sort = req.param('sort');
@@ -73,5 +74,74 @@ router.get('/', function(req, res, next) {
     }
   })
 });
+
+router.post("/addCart",function (req, res, next) {
+  let userId = '123456';
+  let productId = req.body.productId;
+  User.findOne({userId:userId},function (err,userDoc) {
+    if (err) {
+        res.json({
+          status: "1",
+          msg: err.message
+        })
+    } else {
+        if (userDoc) {
+          let userItem = "";
+          userDoc.cartList.forEach(item => {
+            if (item.productId == productId) {
+              userItem = item;
+              item.productNum ++;
+            }
+          })
+          if (userItem) {
+            userDoc.save(function (err2,doc2) {
+              if (err2) {
+                res.json ({
+                  status: "1",
+                  msg: err2.message
+                })
+              } else {
+                res.json({
+                  status: "0",
+                  msg: "",
+                  result: userItem,
+                })
+              }
+            })
+          } else {
+            Goods.findOne({productId:productId},function (err, doc) {
+              if (err) {
+                res.json({
+                  status:"1",
+                  msg: err.message
+                })
+              } else {
+                if (doc) {
+                  var obj = doc.toObject();
+                  obj.productNum = 1;
+                  obj.checked = 1;
+                  userDoc.cartList.push(obj);
+                  userDoc.save(function (err2,doc2) {
+                    if (err2) {
+                      res.json ({
+                        status: "1",
+                        msg: err.message
+                      })
+                    } else {
+                      res.json({
+                        status: "0",
+                        msg: "",
+                        result: doc,
+                      })
+                    }
+                  })
+                }
+              }
+            });
+          }
+        }
+    }
+  });
+})
 
 module.exports = router;
